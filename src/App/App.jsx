1719d15bb4b2addfce5a14cd2c9cb0be894467f5
Bohdan-Ta <81333397+Bohdan-Ta@users.noreply.github.com>
2021-12-13 15:05:23 +0200

    to do Modal

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,43 @@
-function App() {
-  return <div></div>;
-}
+import { Component } from "react";
+// import Button from "../Button";
+import Modal from "../Modal";
+import s from "../Button/Button.module.css";
 
-export default App;
+export default class App extends Component {
+  state = {
+    showModal: false,
+  };
+
+  componentDidMount() {}
+  componentDidUpdate() {}
+  componentWillUnmount() {}
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+
+  render() {
+    const { showModal } = this.state;
+    return (
+      <>
+        {/* <Button /> */}
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <h1>hello</h1>
+            <button
+              className={s.button}
+              type="button"
+              onClick={this.toggleModal}
+            >
+              close Modal
+            </button>
+          </Modal>
+        )}
+
+        <button className={s.button} type="button" onClick={this.toggleModal}>
+          close Modal
+        </button>
+      </>
+    );
+  }
+}
---
+++ src/Button/Button.jsx
@@ -0,0 +1,9 @@
+import s from "./Button.module.css";
+
+export default function Button() {
+  return (
+    <button type="button" className={s.button}>
+      Load more
+    </button>
+  );
+}
